3553de80e70b2e244fd954b8b549a8aca1332cdf
x4v13r100 <j4v13r15@hotmail.com>
2022-11-23 19:33:16 -0500

    middleware (confirmar)

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -8,7 +8,7 @@ const prueba = (req, res) => {
 };
 
 const registrar = async (req, res) => {
-    const { nombre, email} = req.body;
+    const { nombre, email } = req.body;
     // Validar usuario duplicado
     // findOne busca por los diferentes atributos de la coleccion
     const existeUsuario = await Usuario.findOne({ email });
@@ -31,8 +31,33 @@ const registrar = async (req, res) => {
     };
 };
 
+const confirmar = async (req, res) => {
+    // req.params para leer datos de la URL, en este caso token por que asi lo definimos en la ruta
+    const { token } = req.params;
+    const usuarioConfirmar = await Usuario.findOne({ token });
+    // console.log(usuarioConfirmar);
+    // console.log(token);
+    if (!usuarioConfirmar) {
+        const error = new Error("Token no valido");
+        // console.log("Token no valido");
+        return res.status(404).json({ msg: error.message });
+    };
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+        res.json({
+            msg: "Usuario confirmado correctamente"
+        });
+        // console.log("Usuario confirmado correctamente");
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
 
 export {
     prueba,
     registrar,
+    confirmar
 };
